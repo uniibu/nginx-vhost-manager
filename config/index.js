8a46b47d4bd31d47ca5d4f9b62b6e71946e7b4e0
uni <unibtc@gmail.com>
2018-09-07 19:21:06 +0800

    get ip using os.networkInterfaces

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 function getIPAddress() {
-  const interfaces = j;
+  const interfaces = require('os').networkInterfaces();
   for (const iface of Object.values(interfaces)) {
     for (const alias of iface) {
       if (alias.family === 'IPv4' && !alias.internal) {
