e05508a7b3df9a70fd16e142d5ef059a2c9dce5f
vhanni <bayanipsayoiii@gmail.com>
2018-09-07 19:58:11 +0800

    Add Login

--- components/AppAuth.vue
+++ components/AppAuth.vue
@@ -1,26 +1,29 @@
 <template>
   <div class="LoginWrapper">
-    <div class="row d-flex justify-content-around">
-      <div class="container">
-        <div class="card">
-          <div class="card-body">
-            <form>
-              <div class="form-group">
-                <label for="InputPassword1">Password</label>
-                <input 
-                  id="InputPassword" 
-                  type="password" 
-                  class="form-control" 
-                  placeholder="Password">
-              </div>
-              <button 
-                type="submit" 
-                class="btn btn-primary">Submit</button>
-            </form>
+    <div class="container pt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label for="InputPassword1">Password</label>
+                  <input 
+                    id="InputPassword" 
+                    type="password" 
+                    class="form-control" 
+                    placeholder="Password">
+                </div>
+                <button 
+                  type="submit" 
+                  class="btn btn-primary">Submit</button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
     </div>
+   
   </div>
 </template>
 
