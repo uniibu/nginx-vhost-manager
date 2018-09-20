b2290b2fc5b03ec2b7a90ec111edb2092e5f6aba
vhanni <bayanipsayoiii@gmail.com>
2018-09-21 04:26:15 +0800

    fixed login page

--- components/AppAuth.vue
+++ components/AppAuth.vue
@@ -1,30 +1,40 @@
 <template>
-  <div class="LoginWrapper">
-    <div class="container pt-5">
+  <section class="LoginWrapper section-lg my-0">
+    <div class="container pt-lg-md">
       <div class="row justify-content-center">
-        <div class="col-md-4">
-          <div class="card">
-            <div class="card-body">
+        <div class="col-lg-4">
+          <div class="card border-0 shadow bg-light">
+            <div class="card-body  px-lg-4 pt-lg-4">
+              <div class="text-muted text-center mb-3">
+                <small>Login</small>
+              </div>
               <form @submit.prevent="login">
-                <div class="form-group">
-                  <label for="InputPassword1">Login</label>
-                  <input 
-                    id="InputPassword" 
-                    v-model="loginPassword"
+                <div class="form-group input-group input-group-alternative">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="nc-icon nc-lock-circle-open"/>
+                    </span>
+                  </div>
+                  <input
+                    id="InputPassword"
+                    v-model="loginPassword" 
+                    aria-describedby="addon-right addon-left"
                     type="password"
                     class="form-control" 
                     placeholder="Password">
                 </div>
-                <button 
-                  type="submit" 
-                  class="btn btn-primary">Submit</button>
+                <div class="text-center">
+                  <button 
+                    type="submit" 
+                    class="btn mt-4 btn-primary">Sign In</button>
+                </div>
               </form>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </section>
 </template>
 <script>
 export default{
